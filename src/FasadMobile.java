c6d7c7433c3349cd891771f506d167df9112bcda
Overlord7667 <aspiaro@gmail.com>
2023-06-07 19:19:56 +0300

    Modern

--- src/FasadMobile.java
+++ src/FasadMobile.java
@@ -2,7 +2,6 @@ public class FasadMobile {
     public static void main(String[] args) throws Exception {
         SimCardCheck simCardCheck = createSim("On");
         simCardCheck.aktivate();
-        System.out.println();
     }
     static SimCardCheck createSim(String message) throws Exception {
         if (message.equalsIgnoreCase("On")){
